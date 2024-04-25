dab2e22cdce9ec6a38eb67242bb33171abad12fb
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-25 13:14:10 +0300

    <APP> Updated Category Delete Method in Category Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -69,6 +69,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('categories.index');
     }
 }
