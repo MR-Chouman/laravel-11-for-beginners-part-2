69db4b7997565d3e342d9a435cdc05e0dd99d5f0
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-25 11:08:01 +0300

    <RESOURCES> Updated Edit View Style

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -16,10 +16,10 @@
                             <div>
                                 <label for="name">Name:</label>
                             </div>
-                            <input type="text" name="name" id="name" value="{{ $category->name }}">
+                            <input type="text" name="name" id="name" value="{{ $category->name }}" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"> 
                         </div>
                         <div>
-                            <button type="submit">
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"> 
                                 Save
                             </button>
                         </div>
