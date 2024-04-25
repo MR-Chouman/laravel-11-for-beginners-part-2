1c9b560dee5bbbc1581de87387607127bdab26c5
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-25 15:54:16 +0300

    <RESOURCES> Included Posts Resource Route in Is_Admin Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,10 @@ Route::middleware('auth')->group(function () {
         return view('dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::resource('categories', CategoryController::class)->middleware('is_admin');
-
+    Route::middleware('is_admin')->group(function () {
+        Route::resource('categories', CategoryController::class);
+        Route::resource('posts', PostController::class);
+    });
 });
 
 require __DIR__ . '/auth.php';
